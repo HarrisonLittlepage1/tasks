104e08d0ab596a8690b955f5affb93d31e643e20
Harrison Littlepage <harrisonlittlepage@Harrisons-MacBook-Pro-2.local>
2022-02-09 19:42:57 -0500

    added hello world

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript Harrison Littlepage
+                UD CISC275 Hello World Harrison Littlepage
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Hello World
+                automatically reload.
             </p>
         </div>
     );
