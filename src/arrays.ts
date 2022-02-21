c8f827a7bca73100272e5da3950cb1396ad95be3
Harrison Littlepage <harrisonlittlepage@Harrisons-MBP-2.fios-router.home>
2022-02-21 16:23:47 -0500

    wrote function 2 "tripled'

--- src/arrays.ts
+++ src/arrays.ts
@@ -19,7 +19,8 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    const tripled = numbers.map((x: number): number => x * 2);
+    return tripled;
 }
 
 /**
